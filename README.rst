3d2b5eab9b8b6d2ea25472a76dddac644d7a6c38
Morgan Schwartz <msschwartz21@gmail.com>
2017-04-12 14:11:59 -0400

    Updating readme

--- README.rst
+++ README.rst
@@ -1,2 +1,14 @@
-include README.rst
-include
+craniumPy
+##########
+
+This package is designed to quantify biological structures in 3D image data. 
+
+Installation
+++++++++++++
+
+Package hosted on `PyPI <https://pypi.python.org/pypi/cranium>`_. ::
+
+	$ pip install cranium
+
+Basic Usage
+++++++++++++
--- setup.py
+++ setup.py
@@ -5,14 +5,9 @@ def readme():
 		return f.read()
 
 setup(name='cranium',
-	version='0.0.1',
+	version='0.0.2',
 	description='Python package to quantify biological structure',
 	long_description=readme(),
-	classifiers=[
-		'License :: MIT License'
-		'Programming Language :: Python :: 3.5.2',
-		'Topic :: Biology :: Image analysis'
-		],
 	keywords='biology image analysis quantification',
 	url='https://github.com/msschwartz21/craniumPy',
 	author='Morgan Schwartz',
@@ -20,7 +15,7 @@ setup(name='cranium',
 	license='MIT',
 	packages=['cranium'],
 	install_requires=[
-		'os'
+		'markdown'
 		],
 	include_package_data=True
 	)
