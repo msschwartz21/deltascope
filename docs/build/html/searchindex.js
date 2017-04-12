c28a8aaad2ff72e29e72aaa0e2e23445539593b8
Morgan Schwartz <msschwartz21@gmail.com>
2017-04-12 14:25:39 -0400

    Setting up documentation

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:47,filenames:["API","index","introduction"],objects:{},objnames:{},objtypes:{},terms:{content:1,index:1,introduct:1,modul:1,page:1,search:1},titles:["API","Welcome to craniumPy&#8217;s documentation!","Introduction"],titleterms:{api:0,craniumpi:1,document:1,indic:1,introduct:2,tabl:1,welcom:1}})
