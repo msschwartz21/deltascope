1603ed942e20161cd30383a1512df93dacaf2593
Morgan Schwartz <msschwartz21@gmail.com>
2018-01-08 15:59:49 -0500

    Removing import cranium from conf due to error in read the docs build ;

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ import sys
 import os
 import shlex
 
-import cranium
+#import cranium
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
