78df043f79364747366b48e8989e7a9c5addd7b4
Morgan Schwartz <msschwartz21@gmail.com>
2018-01-08 17:56:22 -0500

    Bump version to v0.1.3

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 def readme():
 	with open('README.rst') as f:
-		return f.read()
+		return (f.read())
 
 def requirements():
 	with open('requirements.txt') as f:
-		return f.read()
+		return (f.read())
 
 setup(name='cranium',
-	version='0.1.2',
+	version='0.1.3',
 	description='Python package to quantify biological structure',
 	long_description=readme(),
 	keywords='biology image analysis quantification',
