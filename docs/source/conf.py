1dee67a94752a145ce520523f379e1dc7e2addc8
Morgan Schwartz <msschwartz21@gmail.com>
2018-01-08 17:12:56 -0500

    Shielding c-dependent modules as mock modules ;

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,16 @@ import sys
 import os
 import shlex
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['scipy', 'numpy', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 #import cranium
 
 # If extensions (or modules to document with autodoc) are in another directory,
