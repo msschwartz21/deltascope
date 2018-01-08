32cbe06abc8aa45474452392e99d1299e073f0ae
Morgan Schwartz <msschwartz21@gmail.com>
2018-01-08 16:00:38 -0500

    Bump version number to 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def requirements():
 		return f.read()
 
 setup(name='cranium',
-	version='0.1.0',
+	version='0.1.1',
 	description='Python package to quantify biological structure',
 	long_description=readme(),
 	keywords='biology image analysis quantification',
