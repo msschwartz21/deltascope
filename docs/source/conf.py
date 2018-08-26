07eb07172bcbb4f48792375e7ddbd695053329e8
Morgan Schwartz <msschwartz21@gmail.com>
2018-08-26 15:19:09 -0400

    Add most modeules to mock modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,8 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['scipy', 'numpy', 'pandas','h5py','matplotlib','matplotlib.pyplot']
+MOCK_MODULES = ['scipy', 'numpy', 'pandas','h5py',
+'matplotlib','sklearn','skimage']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 #import deltascope
