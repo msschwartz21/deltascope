0cef268030046d6d9e1e4131103d8f0fc8462098
Morgan Schwartz <msschwartz21@gmail.com>
2019-01-02 13:07:29 -0500

    Update requirments for working installation

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 def requirements():
 	with open('requirements.txt') as f:
-		return (f.read())
+		return (f.read().split('\n'))
 
 setup(name='deltascope',
 	version='0.2.4',
@@ -18,25 +18,6 @@ setup(name='deltascope',
 	author_email='msschwartz21@gmail.com',
 	license='GNU GPL',
 	packages=['deltascope'],
-	install_requires=[
-		'cycler==0.10',
-		'decorator==4.1',
-		'h5py==2.8',
-		'matplotlib>=1.5,<3.0',
-		'networkx==1.11',
-		'numpy>=1.0,<2.0',
-		'pandas>=0.19',
-		'patsy==0.4',
-		'Pillow==5.0',
-		'pyparsing==2.2',
-		'python-dateutil==2.6',
-		'pytz==2017.3',
-		'PyWavelets==0.5',
-		'scikit-image==0.12',
-		'scikit-learn==0.18',
-		'scipy>=0.18',
-		'six==1.11',
-		'statsmodels==0.8'
-	],
+	install_requires=requirements(),
 	include_package_data=True
 	)
